0b91bc8f73c2121131a40f710111148098009138
Liston Cosmas <lcosmas@u.rochester.edu>
2023-08-11 23:55:29 +0300

    Update ui_components_html.py

--- gui/ui_components_html.py
+++ gui/ui_components_html.py
@@ -5,14 +5,12 @@ class GradioComponentsHTML:
         '''Create HTML for the header'''
         return '''
             <div style="display: flex; justify-content: space-between; align-items: center; padding: 5px;">
-            <h1 style="margin-left: 0px; font-size: 35px;">ShortGPT</h1>
+            <h1 style="margin-left: 0px; font-size: 35px;">AI Content Automation</h1>
             <div style="flex-grow: 1; text-align: right;">
-                <a href="https://discord.gg/uERx39ru3R" target="_blank" style="text-decoration: none;">
-                <button style="margin-right: 10px; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #7289DA; border: none; border-radius: 5px; cursor: pointer;">Join Discord</button>
+                <a href="https://support.crmpl.us" target="_blank" style="text-decoration: none;">
+                <button style="margin-right: 10px; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #7289DA; border: none; border-radius: 5px; cursor: pointer;">Create a Support Ticket</button>
                 </a>
-                <a href="https://github.com/RayVentura/ShortGPT" target="_blank" style="text-decoration: none;">
-                <button style="padding: 10px 20px; font-size: 16px; color: #fff; background-color: #333; border: none; border-radius: 5px; cursor: pointer;">Add a Star on Github ⭐</button>
-                </a>
+                
             </div>
             </div>
         '''
@@ -24,7 +22,7 @@ class GradioComponentsHTML:
           <h2 style='margin: 0;'>ERROR : {error_message}</h2>
           <p style='margin: 10px 0;'>Traceback Info : {stack_trace}</p>
           <p style='margin: 10px 0;'>If the problem persists, don't hesitate to contact our support. We're here to assist you.</p>
-          <a href='https://discord.gg/qn2WJaRH' target='_blank' style='background: #a94442; color: #fff; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer; text-decoration: none;'>Get Help on Discord</a>
+          <a href='https://support.crmpl.us' target='_blank' style='background: #a94442; color: #fff; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer; text-decoration: none;'>Create Support Ticket</a>
         </div>
         '''
 
